c8a4e5061d6e8c39b442e31a615fe54e16db862d
Gustavo Serra <g.serra@ihealthlabs.com>
2022-02-19 17:59:29 +0100

    Adding announce model

---
+++ TestTechniqueLeboncoin/Model/Announce.swift
@@ -0,0 +1,25 @@
+//
+//  Announce.swift
+//  TestTechniqueLeboncoin
+//
+//  Created by Gustavo Serra on 19/02/2022.
+//
+
+import Foundation
+
+struct Announce: Codable {
+    let id: Int
+    let title: String
+    let category_id: Int64
+    let creation_date: String
+    let description: String
+    let is_urgent: Bool
+    let images_url: Image
+    let price: Double
+    let siret: String?
+}
+
+enum Image: String, Codable {
+    case small
+    case thumb
+}
