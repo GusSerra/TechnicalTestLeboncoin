04c7e85d7ccc65f8061045befdcb514ea6b47a81
Gustavo Serra <g.serra@ihealthlabs.com>
2022-02-19 18:01:20 +0100

    Adding category model

---
+++ TestTechniqueLeboncoin/Model/Category.swift
@@ -0,0 +1,13 @@
+//
+//  Category.swift
+//  TestTechniqueLeboncoin
+//
+//  Created by Gustavo Serra on 19/02/2022.
+//
+
+import Foundation
+
+struct Category: Codable {
+    let id: Int64
+    let name: String
+}
